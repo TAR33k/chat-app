3db5d664ee61e1eb7c0ae1a905bc16ca2852a3f4
TAR33k <tarik.kukuljac@gmail.com>
2025-12-31 16:35:23 +0100

    Implemented auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 const app = express();
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+  connectDB();
 });
